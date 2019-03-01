bf480faf0587f5177c7559a56ce6dc4eecd77067
Josh <joshfokis@gmail.com>
2019-02-28 20:04:46 -0600

    Update run.py

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
 from turnover import socketio, app
 
+#####################################
+# Turnover
+#
+# Author: Josh Flinner (joshfokis@gmail.com)
+# Organization: Fokis Design
+# Version: 1.5
+# Date: 02/28/2019
+####################################
+
 socketio.run(app, host="0.0.0.0")
