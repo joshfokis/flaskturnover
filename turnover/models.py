8a442cb78b1b637eb23bbdef978b8078c71dd505
Josh <joshfokis@gmail.com>
2019-02-28 21:00:33 -0600

    Updated clients to use one table
    Updated clients to use one table

--- turnover/models.py
+++ turnover/models.py
@@ -3,6 +3,9 @@ from turnover import db
 
 
 class Alerts(db.Model):
+    """
+    This creates the Alerts table in the database.
+    """
     __tablename__ = 'alerts'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(140))
@@ -11,18 +14,41 @@ class Alerts(db.Model):
     startdate = db.Column(db.Date)
     enddate = db.Column(db.Date)
     cleared = db.Column(db.Boolean, default=False)
+    poster = db.Column(db.String(140))
+    priority = db.Column(db.String(140))
+
+class Clients(db.Model):
+    __tablename__= 'client'
+    id = db.Column(db.Integer, primary_key=True)
+    client_name = db.Column(db.String(64), unique=True)
+
+    def __repr__(self):
+        """
+        This returns the data as a string.
+        """
+        return '%s' % (self.client_name)
 
 class Role(db.Model):
+    """
+    This creates the roles table in the database.
+    """
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
     users = db.relationship('User', backref='role', lazy='dynamic')
+    # users = []
 
     def __repr__(self):
+        """
+        This returns the data as a string.
+        """
         return '%s' % (self.name)
 
 
 class User(db.Model):
+    """
+    This creates the user table with the password and role ID.
+    """
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True)
@@ -30,37 +56,67 @@ class User(db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     active = db.Column(db.Boolean, default=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    # role_id = role_id
+    lastlogin = db.Column(db.DateTime)
 
     def __init__(self, username, password, email, active, role_id):
-        self.username = username
+        """
+	    This returns the information from the table 
+        """
+        print('password: {}'.format(password))
+        self.username = username.lower()
         self.pwd_hash = generate_password_hash(password)
         self.email = email
         self.active = active
         self.role_id = role_id
+        # super(User,self).__init__()
 
     @property
     def password(self):
+        """
+        This sets the password as a property
+        """
         raise AttributeError('password is not a readable attribute')
 
     @password.setter
     def password(self, password):
+        """
+        This generates the password hash
+        """
         self.pwd_hash = generate_password_hash(password)
 
     def verify_password(self, password):
+        """
+        This checks the password against the hash
+        """
+        print('hash: {}, password: {}'.format(self.pwd_hash, password))
+        print('password_check: {}'.format(check_password_hash(self.pwd_hash, password)))
         return check_password_hash(self.pwd_hash, password)
 
     def is_authenticated(self):
+        """
+        Returns true if the user is authenticated
+        """
         # return self.authenticated
         return True
 
     def is_active(self):
+        """
+        Returns true if active
+        """
         # return True
         return self.active
 
     def is_anonymous(self):
+        """
+        Checks if user is anonymous
+        """
         return False
 
     def get_id(self):
+        """
+        Try to use the unicode standard or return as string
+        """
         try:
             return unicode(self.id)
         except NameError:
@@ -72,39 +128,17 @@ class User(db.Model):
     def __unicode__(self):
         return '%s' % (self.username)
 
-class EventsClient1(db.Model):
-    __bind_key__ = 'client1'
-    __tablename__ = 'client1'
+class ClientEvents(db.Model):
+    """
+    Creates the Educate events table in the database
+    """
+    # __bind_key__ = 'educate'
+    __tablename__ = 'clientevents'
     id = db.Column(db.Integer, primary_key=True)
     event = db.Column(db.String(500))
     posted = db.Column(db.DateTime)
-    poster = db.Column(db.String, db.ForeignKey('users.id'))
+    poster = db.Column(db.String(50))
+    client = db.Column(db.Integer, db.ForeignKey('client.id'))
 
     def __repr__(self):
-        return '<EventsClient1 %r>' % (self.event)
-
-
-class EventsClient2(db.Model):
-    __bind_key__ = 'client2'
-    __tablename__ ='client2'
-    id = db.Column(db.Integer, primary_key=True)
-    event = db.Column(db.String(500))
-    posted = db.Column(db.DateTime)
-    poster = db.Column(db.String, db.ForeignKey('users.id'))
-
-    def __repr__(self):
-        return '<EventsClient2 %r>' % (self.event)
-
-
-class EventsClient3(db.Model):
-    __bind_key__ = 'client3'
-    __tablename__ ='client3'
-    id = db.Column(db.Integer, primary_key=True)
-    event = db.Column(db.String(500))
-    posted = db.Column(db.DateTime)
-    poster = db.Column(db.String, db.ForeignKey('users.id'))
-
-    def __repr__(self):
-        return '<EventsClient3 %r>' % (self.event)
-
-
+	    return '<ClientEvents %r>' % (self.event)
