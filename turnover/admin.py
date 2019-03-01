f0d8f2fc19270a9efd07bf2dacfd2bc04f934e5d
Josh <joshfokis@gmail.com>
2019-02-28 20:55:29 -0600

    Update admin.py

--- turnover/admin.py
+++ turnover/admin.py
@@ -1,40 +1,58 @@
 from flask import flash, redirect
-from flask.ext.admin.contrib.sqla import ModelView, filters
-from flask.ext.admin.form import rules
-from flask.ext.admin import AdminIndexView, BaseView, expose
+from flask_admin.contrib.sqla import ModelView, filters
+from flask_admin.form import rules
+from flask_admin import AdminIndexView, BaseView, expose
 from werkzeug.security import generate_password_hash
-from flask.ext.wtf import Form
-from flask.ext.login import login_user, logout_user, current_user, login_required
+from flask_wtf import Form
+from flask_login import login_user, logout_user, current_user, login_required
 from wtforms import StringField, BooleanField, TextAreaField, SubmitField, SelectField, PasswordField, validators
 from turnover import admin, db, lm
-from .models import User, Role, EventsClient1, EventsClient2, EventsClient3, Alerts
+from .models import User, Role, ClientEvents, Alerts
 
 
 class UserAdminView(ModelView):
+    """
+    This creates the Admin Dashboard for the Application.
+    """
+    column_labels = dict(lastlogin='Last Login')
     column_searchable_list = ('username',)
-    column_sortable_list = ('username', 'role', 'active')
+    column_sortable_list = ('username', 'role', 'active', 'lastlogin')
     column_exclude_list = ('pwd_hash',)
     form_excluded_columns = ('pwd_hash',)
-    form_edit_rules = ('username', 'email', 'active')
+    form_edit_rules = ('username', 'email', 'active', 'role')
     form_create_rules = ('username', 'password', 'email', 'role', 'active')
     # export_csv()
     # edit_form = AdminUserEditForm
 
     def is_accessible(self):
+        """
+        This Function notes if the application is accessible and to which user roles.
+        """
         return current_user.is_authenticated() and current_user.role.name == 'admin'
 
     def scaffold_form(self):
+        """
+        This Fucntion determines how the fields are viewed.
+        """
         form_class = super(UserAdminView, self).scaffold_form()
         form_class.password = PasswordField('Password')
-        form_class.role = SelectField('Role', choices=[('3', 'Non Cleared'), ('2', 'Cleared'), ('1', 'Admin')])
         return form_class
 
     def create_model(self, form):
+        """
+        Function creates a form for a model, This was a fix to convert a String to an INT.
+        """
+        roledata = None
+        if str(form.role.data) == 'admin':
+            roledata = 1
+        elif str(form.role.data) == 'Cleared':
+            roledata = 2
+
         model = self.model(
             form.username.data, form.password.data,
-            form.email.data, form.role.data, form.active.data
+            form.email.data, form.active.data, roledata
         )
-        form.populate_obj(model)
+        #form.populate_obj(model)
         self.session.add(model)
         self._on_model_change(form, model, True)
         self.session.commit()
@@ -53,54 +71,52 @@ class UserAdminView(ModelView):
 
 
 class RoleAdminView(ModelView):
+    """
+    This class creates the Role view.
+    """
     column_list = ('id', 'name')
 
     def is_accessible(self):
+        """
+        This Function notes if the application is accessible and to which user roles.
+        """
         return current_user.is_authenticated() and current_user.role.name == 'admin'
 
 
 class EventsClient1View(ModelView):
+    """
+    This class creates the Event view for Client 1.
+    """
     column_labels = dict(posted='Timestamp', poster='User')
     column_list =('posted', 'poster', 'event')
-    column_searchable_list = (EventsClient1.event, 'event')
-    column_filters = ('poster', 'posted')
-    column_default_sort = ('posted', True)
-    # column_filters = (BooleanEqualFilter(EventsClient1.poster, 'poster'))
-
-    def is_accessible(self):
-        return current_user.is_authenticated() and current_user.role.name == 'admin'
-
-
-
-class EventsClient2View(ModelView):
-    column_labels = dict(posted='Timestamp', poster='User')
-    column_list =('posted', 'poster', 'event')
-    column_searchable_list = (EventsClient2.event, 'event')
+    column_searchable_list = (ClientEvents.event, 'event')
     column_filters = ('poster', 'posted')
     column_default_sort = ('posted', True)
 
     def is_accessible(self):
-        return current_user.is_authenticated() and current_user.role.name == 'admin'
-
-class EventsClient3View(ModelView):
-    column_labels = dict(posted='Timestamp', poster='User')
-    column_list =('posted', 'poster', 'event')
-    column_searchable_list = (EventsClient3.event, 'event')
-    column_filters = ('poster', 'posted')
-    column_default_sort = ('posted', True)
-
-    def is_accessible(self):
+        """
+        This Function notes if the application is accessible and to which user roles.
+        """
         return current_user.is_authenticated() and current_user.role.name == 'admin'
 
 
 class AlertAdminView(ModelView):
+    """
+    This creates the Alert view.
+    """
     column_searchable_list = (Alerts.details, 'details')
     column_filters = ('client', 'startdate', 'enddate', 'cleared')
     column_default_sort = ('startdate', True)
 
     def is_accessible(self):
+        """
+        This Function notes if the application is accessible and to which user roles.
+        """
         return current_user.is_authenticated() and current_user.role.name == 'admin'
 
+"""
+The below adds the database data to the views.
+"""
 admin.add_view(UserAdminView(User, db.session))
 admin.add_view(RoleAdminView(Role, db.session))
 admin.add_view(AlertAdminView(Alerts, db.session))
